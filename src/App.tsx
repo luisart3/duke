a0ec0380d7bc0427a7f68c137420cd96394ac7ec
luisart3 <luisgerardosart@gmail.com>
2026-03-23 03:37:04 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
 // components
 import { NavBar } from './components/ui/navbar/NavBar';
